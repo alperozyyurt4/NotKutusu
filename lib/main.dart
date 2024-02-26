f366623d643fe5aaa1c2d149e29c1e7b4fbfcd13
Enes Cümbüş <ecumbus29@gmail.com>
2024-02-27 01:07:07 +0300

    u

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
       home: const Placeholder(),
     );
