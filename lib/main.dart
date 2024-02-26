3c506a79542c0e646e6e10af6ab925c2d9fe814c
Alper Ozyurt <alperozyyurt@gmail.com>
2024-02-27 01:05:45 +0300

    y

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const Placeholder(),
     );
