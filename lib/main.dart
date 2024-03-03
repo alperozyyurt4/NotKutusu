0a576818c49270edfdae67e9ed74a90823757947
alperozyyurt4 <alperozyyurt@gmail.com>
2024-03-04 00:13:58 +0300

    firebase bağlantısı kesildi

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:notkutusu/LoginPage/login_page.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  print('Firebase bağlandı');
-
   runApp(const MyApp());
 }
 
