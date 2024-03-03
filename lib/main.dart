024ba343ae604d4285cf3acf4b21835612f232cf
Enes Cümbüş <ecumbus29@gmail.com>
2024-03-04 00:32:36 +0300

    new firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:notkutusu/LoginPage/login_page.dart';
+import 'package:notkutusu/firebase_options.dart';
 
 void main() async {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  print('Firebase bağlandı');
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
